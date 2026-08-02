cecedd535d40dbf905ca21c6814237e8fa0e0b3c
Kirthika S <kirthika0816@gmail.com>
2026-08-02 14:30:46 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,11 +6,11 @@ from mysql.connector import Error
 # Update these values to match your local setup
 # ---------------------------------------------
 DB_CONFIG = {
-    'host': 'MYSQLHOST value',
+    'host': 'mysql.railway.internal',
     'port': 3306,
-    'user': 'MYSQLUSER value',
-    'password': 'MYSQLPASSWORD value',
-    'database': 'MYSQLDATABASE value'
+    'user': 'root',
+    'password': 'yuQwPkPRwxlTVZOFhRLHTvrJUuZyNGcF',
+    'database': 'railway'
 }
 
 

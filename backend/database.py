1bd80304f62db11734a27e6b525bda23d662ef22
Kirthika S <kirthika0816@gmail.com>
2026-08-24 18:24:36 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,7 @@ from mysql.connector import Error
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
+    "port": int(os.getenv("DB_PORT","3306")),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),
